b19906a2119d5fdddfa4345cdbb58c190b4d6f1a
sharmarke1 <sharmarkeibrahim@Sharmarke-Ibrahims-MacBook-Pro.local>
2017-05-10 16:23:53 -0700

    improved loops

--- app.js
+++ app.js
@@ -1,59 +1,50 @@
 'use strict'
 
-// Start
+  // Start
+
 alert('Hello! Let\'s play a little game!');
 
 // Get Player's Name
+
 var user = prompt('First things first, What\'s you\'re name?');
 console.log('user name:' + user);
 
 alert('Thanks ' + user + ' ! Let\'s get started!');
 
-// First Question
-var shoeType = ['Nike', 'Asics', 'Vans']
+  // First Question
+
 var shoe = prompt('What is my favorite shoe brand?(Vans/Nike/Asics?)').toUpperCase();
-var flagOne;
+
 console.log('questionOne shoes:' + shoe);
 
-for (var i = 0; i < shoeType.length; i++) {
-  console.log('shoeType:', shoeType[i]);
+while (shoe != 'NIKE') {
+  alert('Wrong! Try again');
+  var shoe = prompt('What is my favorite shoe brand?(Vans/Nike/Asics?)').toUpperCase();
+}
+alert('Correct!');
 
-  if (shoe === shoeType[i]) {
-    alert('YES! That\'s the one!')
-    flag = true;
-    break;
+// Second Question
+
+var houseArray = ['SEATTLE', 'TACOMA', 'BELLEVUE'];
+var house = prompt('What are my favorite cities in Washington?').toUpperCase();
+var first;
+
+for (var i = 0; i < houseArray.length; i++) {
+  console.log('houseArray:', houseArray[i]);
+
+  if (house === houseArray[i] ) {
+  alert('One of my favorite cities!');
+  first = true;
+  break;
   }
 }
 
-if (!flagOne) {
-  alert('Nope! Wrong one')
-}
-//   if (shoe === 'VANS') {
-//     alert('Nope! You\'re wrong.');
-//   } else if (shoe === 'NIKE') {
-//     alert('Yup, that\'s the one!');
-//   } else if (shoe === 'ASICS') {
-//     alert('I wouldn\'t be caught dead wearing them!');
-//   } else {
-//     alert('What are those?');
-//     flagOne = true;
-//     break;
-//   }
-// }
-
-// }
-
-// Second Question
-var house = prompt('Do I have my own place?').toUpperCase();
-console.log('own house:' + house);
-
-if (house === 'Y' || house === 'YES') {
-  alert('I wish! Still at momma\'s house.');
-} else if (house === 'N' || house === 'NO') {
-  alert('Hopefully after landing a job with what I learn at CF!');
+if(!first) {
+alert('Nahh, that city sucks.');
 }
 
-// Third Question
+  // Third Question
+
 var height = prompt('Am I taller than 6 feet?').toUpperCase();
 console.log('my height:' + height);
 
@@ -62,20 +53,29 @@ if (height === 'Y' || height === 'YES') {
 } else if (height === 'N' || height === 'NO') {
   alert('Yeaaa, I\'m just an average joe');
 } else {
-  alert('Dude, only answer Y/N')
+  alert('Dude, only answer Y/N');
 }
 
-// Fourth Question
-var computer = prompt('Do I use a Mac computer?').toUpperCase();
+  // Fourth Question
+
+var comupterChoice = ['MAC', 'MACBOOK', 'MAC BOOK', 'MACBOOK PRO', 'APPLE', 'MAC BOOK PRO']
+var computer = prompt('What kinda computer do I prefer?').toUpperCase();
+var comp;
+
+for (var i = 0; i < comupterChoice.length; i++) {
+  console.log('computerChoice:', comupterChoice[i]);
+}
 console.log('type computer:' + computer);
 
-if(computer === 'Y' || computer === 'YES'){
-  alert('Apple squad!')
-} else{
-  alert('PC\'s are extinct.')
+if(computer === comupterChoice) {
+  alert('Apple squad!');
+} else {
+  alert('PC\'s are extinct.');
+  comp = true;
 }
 
-// Fifth Question
+  // Fifth Question
+
 var workout = prompt('Do I workout?').toUpperCase();
 console.log('my workout:' + workout);
 
@@ -86,6 +86,7 @@ if(workout === 'Y' || workout === 'YES') {
 }
 
 // Sixth Question
+
 var drink = prompt('Do I drink water?').toUpperCase();
 console.log('choice drink:' + drink);
 
@@ -96,6 +97,7 @@ if (drink === 'Y' || drink === 'YES') {
 }
 
 // Seventh Question
+
 var game = prompt("How would you rate this game? (Bad? Okay? Awesome?)").toUpperCase();
 console.log('answer game:' + game);
 
